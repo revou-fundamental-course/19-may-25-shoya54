9153fa22e1866aea9544cf9f304682389b148279
shoya54 <hosea.ismanto@gmail.com>
2025-05-28 18:11:59 +0700

    Updated

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,35 @@
-function doKonversi() {
 
+$('#formKonversi').on('submit', function (ev) {
+  ev.preventDefault();
+  var regexInput = /^(\+|\-)?\d+(\,\d+)?$/
+  var inputSuhu = this.inputKonversi.value;
+  customReset();
+  if (regexInput.test(inputSuhu)) {
+    $('#notifikasi').addClass('alert-success');
+    $('#notifikasi i').prop('class', 'bi bi-people');
+    $('#notifikasi span').html(inputSuhu);
+  } else {
+    $('#notifikasi').addClass((inputSuhu.length > 0) ? 'alert-danger' : 'alert-warning');
+    $('#notifikasi i').prop('class', (inputSuhu.length > 0) ? 'bi bi-x-octagon-fill' : 'bi bi-exclamation-diamond-fill');
+    $('#notifikasi span').html((inputSuhu.length > 0) ? 'Input salah.' : 'Input suhu belum diisi.');
+    $('#notifikasi').show();
+    $(this).find('input[name="inputKonversi"]').addClass('is-invalid').focus();
+  }
+});
+
+$('#formKonversi').bind('reset', customReset);
+
+function customReset() {
+  $('#notifikasi').hide();
+  $('#notifikasi').prop('class', 'alert');
+  $('#notifikasi i').prop('class', '');
+  $('#notifikasi span').html('');
+  $('#inputKonversi').removeClass('is-invalid');
+}
+
+function doKonversi(f) {
+  f.preventDefault();
+  console.log(f);
 }
 
 function doReset() {
